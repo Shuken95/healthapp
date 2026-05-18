f4576125744fab8115af96cb90c2deb210a2b23e
Shuken95 <76523054+Shuken95@users.noreply.github.com>
2026-05-18 17:09:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  manual controlada por el usuario (banner)
 // ══════════════════════════════════════════════════════════════
 
-const APP_VERSION   = 'healthapp-v30';
+const APP_VERSION   = 'healthapp-v31';
 const CACHE_STATIC  = `${APP_VERSION}-static`;
 const CACHE_DYNAMIC = `${APP_VERSION}-dynamic`;
 
